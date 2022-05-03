a8d0e4691d6165505b00fd537af54952afabf748
tweedy15 <103071277+tweedy15@users.noreply.github.com>
2022-05-03 17:02:14 -0500

    Add files via upload

--- shoppingCart.py
+++ shoppingCart.py
@@ -8,7 +8,7 @@ class shoppingCart:
         host="localhost",
         user="root",
         password="",
-        database="")
+        database="") ## Needs Database name
         print("Successful connection.")
 
     except:
@@ -16,29 +16,36 @@ class shoppingCart:
 
     ## Creates the cart table
         cursor = connection.cursor()
-        cursor.execute("CREATE TABLE cart ()")
+        cursor.execute("CREATE TABLE cart (ID int NOT NULL, Title varchar(25), Price int)")
         connection.commit()
 
     ## Prints the current items in the table
     def viewCart():
+        query = "SELECT * FROM cart WHERE Title=%s"
+        data = getTitle()
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM cart")
+        cursor.execute(query, data)
         connection.commit()
 
     ## Emties the cart and records the contents in order history
     def checkoutCart():
+        ## Needs to record contents of cart
         cursor = connection.cursor()
         cursor.execute("DROP TABLE cart")
         connection.commit()
 
     ## Adds queried item to cart
-    def addToCart(item):
+    def addToCart():
+        query = "INSERT INTO cart (ID, Title, Price) Values (%s, %s, %s)"
+        data = (getID(), getTitle(), getPrice())
         cursor = connection.cursor()
-        cursor.execute("INSERT INTO cart (Columns) Values ()")
+        cursor.execute(query, data)
         connection.commit()
 
     ## Removes queried item from cart
-    def removeFromCart(item):
-        cursor = connection.cursor()
-        cursor.execute("DELETE FROM cart")
+    def removeFromCart():
+        query = "DELETE FROM cart WHERE Title=%s"
+        data = getTitle()
+        cursor = connection.cursor(query, data)
+        cursor.execute()
         connection.commit()
