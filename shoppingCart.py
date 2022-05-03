d39e46aa4105f9c20016db0c19efe2e7cad88529
tweedy15 <103071277+tweedy15@users.noreply.github.com>
2022-05-03 12:26:43 -0500

    Add files via upload

--- shoppingCart.py
+++ shoppingCart.py
@@ -1,43 +1,44 @@
 import mysql.connector
 import sys
 
-## Connects to the database
-try:
-    connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="",
-    database="")
-    print("Successful connection.")
+class shoppingCart:
+    ## Connects to the database
+    try:
+        connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="",
+        database="")
+        print("Successful connection.")
 
-except:
-    print("Failed connection.")
+    except:
+        print("Failed connection.")
 
-## Creates the cart table
-    cursor = connection.cursor()
-    cursor.execute("CREATE TABLE cart ()")
-    connection.commit()
+    ## Creates the cart table
+        cursor = connection.cursor()
+        cursor.execute("CREATE TABLE cart ()")
+        connection.commit()
 
-## Prints the current items in the table
-def viewCart():
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM cart")
-    connection.commit()
+    ## Prints the current items in the table
+    def viewCart():
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM cart")
+        connection.commit()
 
-## Emties the cart and records the contents in order history
-def checkoutCart():
-    cursor = connection.cursor()
-    cursor.execute("DROP TABLE cart")
-    connection.commit()
+    ## Emties the cart and records the contents in order history
+    def checkoutCart():
+        cursor = connection.cursor()
+        cursor.execute("DROP TABLE cart")
+        connection.commit()
 
-## Adds queried item to cart
-def addToCart(item):
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO cart (Columns) Values ()")
-    connection.commit()
+    ## Adds queried item to cart
+    def addToCart(item):
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO cart (Columns) Values ()")
+        connection.commit()
 
-## Removes queried item from cart
-def removeFromCart(item):
-    cursor = connection.cursor()
-    cursor.execute("DELETE FROM cart")
-    connection.commit()
+    ## Removes queried item from cart
+    def removeFromCart(item):
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM cart")
+        connection.commit()
