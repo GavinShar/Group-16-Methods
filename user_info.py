6a22dcb2312d6c60b9fd95abb0b9d5b83f0d4a9f
connorblack3 <65261129+connorblack3@users.noreply.github.com>
2022-05-03 21:29:50 -0500

    Add files via upload

--- user_info.py
+++ user_info.py
@@ -16,10 +16,10 @@ class user_info:
         print("Failed connection.")
 
 
-    def order_history_add (self):
+    def order_history_add (self, id, transacDate, user_id, isbn):
 
         query = "INSERT INTO history (id, transacDate, userID, ISBN) Values (%s, %s, %s, %s)"
-        data = id, "6/9/6969", userID, isbn
+        data = id, transacDate, user_id, isbn
         cursor = connection.cursor()
         cursor.execute(query, data)
         connection.commit()
@@ -29,8 +29,71 @@ class user_info:
         query = "SELECT * FROM history"
         cursor = connection.cursor()
         cursor.execute(query)
+        result = cursor.fetchall()
+
         connection.commit()
 
-    def user_login(self):
+    def user_login(self, userID):
 
-        query = "SELECT userID"
+        query = "SELECT userPass FROM user_info WHERE userID = %s"
+        cursor = connection.cursor()
+        cursor.execute(query, userID)
+        result = cursor.fetchall()
+
+        if (result != userPass):
+            print ("Invalid Password")
+
+    def edit_login(self, userID, userPass):
+
+        query = "UPDATE user_info SET userPass = '%s' WHERE userID = '%s'"
+        cursor = connection.cursor()
+        cursor.execute(query, userPass, userID)
+        connection.commit()
+
+    def edit_payment (self, userID, cardNum, cardExp, cardCVV):
+
+        query = "UPDATE user_info SET cardNum = '%s' WHERE userID = '%s'"
+        cursor = connection.cursor()
+        cursor.execute(query, cardNum, userID)
+        connection.commit()
+        query = "UPDATE user_info SET cardExp = '%s' WHERE userID = '%s'"
+        cursor = connection.cursor()
+        cursor.execute(query, cardExp, userID)
+        connection.commit()
+        query = "UPDATE user_info SET cardCVV = '%s' WHERE userID = '%s'"
+        cursor = connection.cursor()
+        cursor.execute(query, cardCVV, userID)
+        connection.commit()
+
+    def edit_shipping(self, userID, address, city, state, zip):
+
+        query = "UPDATE user_info SET address = '%s' WHERE userID = '%s'"
+        cursor = connection.cursor()
+        cursor.execute(query, address, userID)
+        connection.commit()
+        query = "UPDATE user_info SET city = '%s' WHERE userID = '%s'"
+        cursor = connection.cursor()
+        cursor.execute(query, city, userID)
+        connection.commit()
+        query = "UPDATE user_info SET cardNum = '%s' WHERE userID = '%s'"
+        cursor = connection.cursor()
+        cursor.execute(query, state, userID)
+        connection.commit()
+        query = "UPDATE user_info SET zip = '%s' WHERE userID = '%s'"
+        cursor = connection.cursor()
+        cursor.execute(query, zip, userID)
+        connection.commit()
+
+    def delete_account (self, userID):
+
+        query = "DELETE FROM user_info WHERE userID = '%s'"
+        cursor = connection.cursor()
+        cursor.execute(query, userID)
+        connection.commit()
+
+    def create_user (self, userID, userPass, cardNum, cardExp, cardCVV, address, city, state, zip):
+
+        query = "INSERT INTO user_info (userID, userPass, cardNum, cardExp, cardCVV, address, city, state, zip) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        cursor = connection.cursor()
+        cursor.execute(query, userID, userPass, cardNum, cardExp, cardCVV, address, city, state, zip)
+        connection.commit()
